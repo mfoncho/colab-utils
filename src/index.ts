804ed3cb74a93bac927c513c84771da928c64458
mfoncho <akonumboche@gmail.com>
2021-02-14 05:23:04 -0600

    exported Theme

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export * from './hooks';
-export { default as theme, createTheme, makeStyles, useTheme } from './theme';
+export { default as theme, createTheme, makeStyles, useTheme, Theme } from './theme';
 export { default as  colors, presence } from './colors';
